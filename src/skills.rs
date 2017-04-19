17db1df28209ca12e58ccd33d9ac33f185b44070
Dustin Withers <dustin.withers@one.verizon.com>
2017-04-19 13:05:45 -0700

    Cascade from skill and skills from cascade

--- src/skills.rs
+++ src/skills.rs
@@ -23,6 +23,37 @@ fn education_skills() -> Vec<Skill> {
          SpaceSciences]
 }
 
+fn cascade_from_skill(skill: Skill) -> Skill {
+    use skills::Skill::*;
+    match skill {
+        Farming | Riding | Survival | VeterinaryMedicine => Animals,
+        LifeSciences | PhysicalSciences | SocialSciences | SpaceSciences => Sciences,
+        Archery | EnergyPistol | EnergyRifle | Shotgun | SlugPistol | SlugRifle => GunCombat,
+        BayWeapons | HeavyWeapons | Screens | SpinalMounts | TurretWeapons => Gunnery,
+        BludgeoningWeapons | NaturalWeapons | PiercingWeapons | SlashingWeapons => MeleeCombat,
+        Aircraft | Mole | Watercraft | WheeledVehicle => Vehicle,
+        GravVehicle | RotorAircraft | WingedAircraft => Aircraft,
+        Motorboats | OceanShips | SailingShips | Submarine => Watercraft,
+        _ => skill
+    }
+}
+
+fn skills_from_cascade(skill: Skill) -> Vec<Skill> {
+    use skills::Skill::*;
+
+    match skill {
+        Animals => vec![Farming, Riding, Survival, VeterinaryMedicine],
+        Sciences => vec![LifeSciences, PhysicalSciences, SocialSciences, SpaceSciences],
+        GunCombat => vec![Archery, EnergyPistol, EnergyRifle, Shotgun, SlugPistol, SlugRifle],
+        Gunnery => vec![BayWeapons, HeavyWeapons, Screens, SpinalMounts, TurretWeapons],
+        MeleeCombat => vec![BludgeoningWeapons, NaturalWeapons, PiercingWeapons, SlashingWeapons],
+        Vehicle => vec![Aircraft, Mole, Watercraft, WheeledVehicle],
+        Aircraft => vec![GravVehicle, RotorAircraft, WingedAircraft],
+        Watercraft => vec![Motorboats, OceanShips, SailingShips, Submarine],
+        _ => Vec::new()
+    }
+}
+
 struct CharacterSkills {
     pub entity_id: u32,
     pub skills: HashMap<Skill, u8>,
