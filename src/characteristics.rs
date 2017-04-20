e287292b0fb74a05f62068ad1a3aca8b5d67751a
Dustin Withers <dustin.withers@one.verizon.com>
2017-04-19 22:20:02 -0700

    characteristics modifier

--- src/characteristics.rs
+++ src/characteristics.rs
@@ -13,6 +13,23 @@ pub struct Characteristics {
     pub social: u8,
 }
 
+pub fn characteristic_modifier(num: u8) -> i8 {
+    match num {
+        x if x >= 0 && x <= 2 => -2,
+        x if x >= 3 && x <= 5 => -1,
+        x if x >= 6 && x <= 8 => 0,
+        x if x >= 9 && x <= 11 => 1,
+        x if x >= 12 && x <= 14 => 2,
+        x if x >= 15 && x <= 17 => 3,
+        x if x >= 18 && x <= 20 => 4,
+        x if x >= 21 && x <= 23 => 5,
+        x if x >= 24 && x <= 26 => 6,
+        x if x >= 27 && x <= 29 => 7,
+        x if x >= 30 && x <= 32 => 8,
+        _ => 9
+    }
+}
+
 impl fmt::Display for Characteristics {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         use pseudo_hex::num_to_pseudo_hex;
@@ -26,36 +43,6 @@ impl fmt::Display for Characteristics {
         let upp = chars.iter().map(|&n| num_to_pseudo_hex(n)).collect::<String>();
         write!(f, "{}", upp)
     }
-}
-
-#[test]
-fn test_characteristics_display() {
-    let char1 =  Characteristics {
-        entity_id: 0,
-        strength: 1,
-        dexterity: 2,
-        endurance: 3,
-        intelligence: 4,
-        education: 5,
-        social: 6,
-    };
-
-    let char1Fmt = format!("{}", char1);
-    assert_eq!(char1Fmt, "123456");
-
-        let char2 =  Characteristics {
-        entity_id: 0,
-        strength: 10,
-        dexterity: 11,
-        endurance: 12,
-        intelligence: 11,
-        education: 10,
-        social: 9,
-    };
-
-    let char2Fmt = format!("{}", char2);
-    assert_eq!(char2Fmt, "ABCBA9");
-
 }
 
 pub enum Characteristic {
@@ -126,4 +113,34 @@ impl Characteristics {
 
         Ok(result)
     }
+}
+
+#[test]
+fn test_characteristics_display() {
+    let char1 =  Characteristics {
+        entity_id: 0,
+        strength: 1,
+        dexterity: 2,
+        endurance: 3,
+        intelligence: 4,
+        education: 5,
+        social: 6,
+    };
+
+    let char1Fmt = format!("{}", char1);
+    assert_eq!(char1Fmt, "123456");
+
+        let char2 =  Characteristics {
+        entity_id: 0,
+        strength: 10,
+        dexterity: 11,
+        endurance: 12,
+        intelligence: 11,
+        education: 10,
+        social: 9,
+    };
+
+    let char2Fmt = format!("{}", char2);
+    assert_eq!(char2Fmt, "ABCBA9");
+
 }
